e4a3f2d0db9d605a36c44de46d6dc5015773d210
Ricardo Sousa <rwmsousa@gmail.com>
2025-01-28 16:02:35 -0300

    fix: modify payment migration to create table if not exists and drop table if exists

--- src/migrations/1738000108350-migration.ts
+++ src/migrations/1738000108350-migration.ts
@@ -4,14 +4,21 @@ export class Migration1738000108350 implements MigrationInterface {
   name = 'Migration1738000108350';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    if (!queryRunner.query(`SELECT * FROM "payment"`)) {
-      await queryRunner.query(
-        `CREATE TABLE "payment" ("id" SERIAL NOT NULL, "name" character varying(15) NOT NULL DEFAULT ' ', "age" character varying(4) NOT NULL DEFAULT '0000', "address" character varying(34) NOT NULL DEFAULT ' ', "cpf" character varying(11) NOT NULL DEFAULT '00000000000', "amountPaid" character varying(16) NOT NULL DEFAULT '0000000000000000', "birthDate" character varying(8) NOT NULL DEFAULT '00000000', CONSTRAINT "PK_fcaec7df5adf9cac408c686b2ab" PRIMARY KEY ("id"))`,
-      );
-    }
+    await queryRunner.query(
+      `CREATE TABLE IF NOT EXISTS "payment" (
+        "id" SERIAL NOT NULL,
+        "name" character varying(15) NOT NULL DEFAULT ' ',
+        "age" character varying(4) NOT NULL DEFAULT '0000',
+        "address" character varying(34) NOT NULL DEFAULT ' ',
+        "cpf" character varying(11) NOT NULL DEFAULT '00000000000',
+        "amountPaid" character varying(16) NOT NULL DEFAULT '0000000000000000',
+        "birthDate" character varying(8) NOT NULL DEFAULT '00000000',
+        CONSTRAINT "PK_fcaec7df5adf9cac408c686b2ab" PRIMARY KEY ("id")
+      )`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "payment"`);
+    await queryRunner.query(`DROP TABLE IF EXISTS "payment"`);
   }
 }
